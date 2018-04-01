831ab2751a6e2de88b21c344d0228e3b5ba798c9
androcore <34034874+androcore@users.noreply.github.com>
2018-04-02 01:01:39 +0700

    Update apkredo-safelink.js

--- apkredo-safelink.js
+++ apkredo-safelink.js
@@ -132,7 +132,8 @@ function auto_safeconvert() {
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
             a_to_ve[i].href = "https://url.apkredo.com/get/?url=" + encryptedUrl;
-            a_to_ve[i].rel = "nofollow";
+            a_to_ve[i].rel = "nofollow noopener";
+            a_to_ve[i].target = "_blank";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
         }
